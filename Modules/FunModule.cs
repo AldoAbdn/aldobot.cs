440f4f3af4ddab7bfbd1e8984846bfafb20699e0
Alistair Quinn <alistairaldo@hotmail.co.uk>
2025-04-24 21:29:21 +0100

    Trying to get !ping to work

---
+++ Modules/FunModule.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Commands;
+
+namespace aldobot.Modules
+{
+    public class FunModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        [Summary("Ping!")]
+        public async Task Ping()
+        {
+            await ReplyAsync("pong");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
-﻿using Discord;
+﻿using aldobot;
+using Discord;
 using Discord.WebSocket;
 
 internal class Program
 {
     private static DiscordSocketClient? _client;
+    private static CommandHandler? _commandHandler;
 
     private static async Task Main()
     {
         _client = new DiscordSocketClient();
         _client.Log += Log;
+        _commandHandler = new CommandHandler(_client, new Discord.Commands.CommandService());
+        await _commandHandler.InstallCommandsAsync();
         string token = Environment.GetEnvironmentVariable("DISCORD_TOKEN") ?? throw new InvalidOperationException("Token not found in environment variables.");
         await _client.LoginAsync(TokenType.Bot, token);
         await _client.StartAsync();
