04ab5589db8a3ad09a2524d8ba5eeb61580120ed
Alistair Quinn <alistairaldo@hotmail.co.uk>
2025-04-25 22:26:32 +0100

    Writing crap code to get tiktokclient to connect

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ internal class Program
     private static DiscordHandler? _discordHandler;
     private static TikTokLiveClient? _tikTokLiveClient;
     private static TikTokLiveHandler? _tiktokHandler;
+    private static ClientSettings _tikTokClientSettings = new ClientSettings();
 
     private static async Task Main()
     {
@@ -20,10 +21,7 @@ internal class Program
         };
         _discordClient = new DiscordSocketClient(config);
         // TikTok
-        var clientSettings = new ClientSettings();
-        clientSettings.SkipRoomInfo = true; 
-        string userName = Environment.GetEnvironmentVariable("TIKTOK_USERNAME") ?? throw new InvalidOperationException("Username not found in environment variables.");
-        _tikTokLiveClient = new TikTokLiveClient(userName, null, null, null, "");
+        SetupTikTokLiveClient();
         // Setup
         await SetupHandlers(_discordClient, _tikTokLiveClient);
         await Run(_discordClient, _tikTokLiveClient);
@@ -38,11 +36,38 @@ internal class Program
         _tiktokHandler = new TikTokLiveHandler(tikTokLiveClient, discordClient);
     }
 
+    private static void SetupTikTokLiveClient()
+    {
+        _tikTokClientSettings.SkipRoomInfo = true;
+        _tikTokClientSettings.RetryOnConnectionFailure = true;
+        string userName = Environment.GetEnvironmentVariable("TIKTOK_USERNAME") ?? throw new InvalidOperationException("Username not found in environment variables.");
+        _tikTokLiveClient = new TikTokLiveClient(userName, "");
+    }
+
+    private static async Task StartTikTok()
+    {
+        await Task.Run(async () =>
+        {
+            while (true)
+            {
+                try
+                {
+                    await _tikTokLiveClient?.RunAsync();
+                }
+                catch (Exception)
+                {
+                    SetupTikTokLiveClient();
+                    await Task.Delay(5000); // Wait before retrying
+                }
+            }
+        });
+    }
+
     private static async Task Run(DiscordSocketClient discordClient, TikTokLiveClient tikTokLiveClient)
     {
         string token = Environment.GetEnvironmentVariable("DISCORD_TOKEN") ?? throw new InvalidOperationException("Token not found in environment variables.");
         await discordClient.LoginAsync(TokenType.Bot, token);
         await discordClient.StartAsync();
-        await tikTokLiveClient.RunAsync();
+        await StartTikTok();
     }
 }
