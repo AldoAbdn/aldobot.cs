61bc8c88cdf632fcfb6091f58de2dc1da0c28805
Alistair Quinn <alistairaldo@hotmail.co.uk>
2025-04-25 22:49:54 +0100

    Dummy commands for admin until i fill them

---
+++ Modules/AdminModule.cs
@@ -0,0 +1,28 @@
+﻿using Discord.Commands;
+
+namespace aldobot.Modules
+{
+    public class AdminModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("admin")]
+        [Summary("Admin commands")]
+        public async Task Admin()
+        {
+            await ReplyAsync("Admin commands");
+        }
+
+        [Command("ban")]
+        [Summary("Ban a user")]
+        public async Task Ban([Remainder] string user)
+        {
+            await ReplyAsync($"Banned {user}");
+        }
+
+        [Command("kick")]
+        [Summary("Kick a user")]
+        public async Task Kick([Remainder] string user)
+        {
+            await ReplyAsync($"Kicked {user}");
+        }
+    }
+}
