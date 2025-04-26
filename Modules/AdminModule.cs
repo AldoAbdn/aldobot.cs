beeb0855c5bb3679a3b455b69a53fa1f3a75f594
Alistair Quinn <alistairaldo@hotmail.co.uk>
2025-04-26 09:30:07 +0100

    Adding permissions to admin module

--- Modules/AdminModule.cs
+++ Modules/AdminModule.cs
@@ -7,18 +7,29 @@ namespace aldobot.Modules
     {
         [Command("ban")]
         [Summary("Ban a user")]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        [RequireBotPermission(GuildPermission.BanMembers)]
         public async Task Ban(IGuildUser user, string reason = "", int days = 0)
         {
             await Context.Guild.AddBanAsync(user, days, reason);
-            await ReplyAsync($"Banned {user}");
         }
 
         [Command("unban")]
         [Summary("Unban a user")]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        [RequireBotPermission(GuildPermission.BanMembers)]
         public async Task Unban(IGuildUser user)
         {
             await Context.Guild.RemoveBanAsync(user);
-            await ReplyAsync($"Unbanned {user}");
+        }
+
+        [Command("kick")]
+        [Summary("Kick a user")]
+        [RequireUserPermission(GuildPermission.KickMembers)]
+        [RequireBotPermission(GuildPermission.KickMembers)]
+        public async Task Kick(IGuildUser user, string reason = "")
+        {
+            await user.KickAsync(reason);
         }
     }
 }
